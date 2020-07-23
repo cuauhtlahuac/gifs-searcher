562cb40c35438a14f80158be9a1051bfbbbd2d3c
Rodrigo <cuitlagua@gmail.com>
2020-07-23 13:25:47 -0500

    Updates

---
+++ precache-manifest.bb0cae205cae2b330d4b6f7016f1ed4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c0fe6e0bf8c9d6d4a8966435a9c1c1c",
+    "url": "/gifs-searcher/index.html"
+  },
+  {
+    "revision": "27d470d23977c5e1833a",
+    "url": "/gifs-searcher/static/css/main.666fe0d9.chunk.css"
+  },
+  {
+    "revision": "12114c4ed8e27cc180ca",
+    "url": "/gifs-searcher/static/js/2.0092fab3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gifs-searcher/static/js/2.0092fab3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2b609d0bcd06584ed38",
+    "url": "/gifs-searcher/static/js/3.9d399cac.chunk.js"
+  },
+  {
+    "revision": "27d470d23977c5e1833a",
+    "url": "/gifs-searcher/static/js/main.a7cc7e19.chunk.js"
+  },
+  {
+    "revision": "1d815bd97ea5b97a6b85",
+    "url": "/gifs-searcher/static/js/runtime-main.5dba864a.js"
+  }
+]);
